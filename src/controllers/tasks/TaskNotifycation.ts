55e7e24f110297fba9e83af2252daf55af75a65c
calangthang1406 <90557694+carp-calangthang@users.noreply.github.com>
2024-09-08 23:56:56 +0700

    create notification list api for dashboard

---
+++ src/controllers/tasks/TaskNotifycation.ts
@@ -0,0 +1,53 @@
+
+import prisma from "../../utils/connection/connection";
+
+type Variable = {
+  skip: number,
+  take: number,
+}
+
+const NotificationList = async ({ skip, take }: Variable) => {
+  try {
+
+    const startOfToday = new Date();
+    startOfToday.setHours(0, 0, 0, 0);
+
+    const changesToday = await prisma.taskHistory.findMany({
+      where: {
+        createdAt: {
+          gte: startOfToday,
+        }
+      },
+      orderBy: {
+        createdAt: 'desc',
+      }
+    })
+    if (changesToday.length === 0) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: 'No changes today' }),
+        data: []
+      }
+    }
+    const changes = changesToday.map((history) => ({
+      taskId: history.taskId,
+      changes: history.changes,
+      action: history.action,
+      createdAt: history.createdAt
+    }));
+    
+    return {
+      statusCode: 200,
+      message: "Load notification",
+      data: JSON.stringify(changes),
+    }
+  } catch (err) {
+    console.error(err);
+    return {
+      status: 500,
+      message: "Internal Server Error",
+    }
+  }
+}
+
+export { NotificationList }
