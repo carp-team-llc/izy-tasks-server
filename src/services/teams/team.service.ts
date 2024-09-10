a5289f56d5ea91075ed9f9584228ca20ded06b05
calangthang1406 <90557694+carp-calangthang@users.noreply.github.com>
2024-09-10 11:32:18 +0700

    build team api and add team member

---
+++ src/services/teams/team.service.ts
@@ -0,0 +1,62 @@
+import { Request, Response } from "express";
+import { CreateTeam, DeleteTeam, UpdateTeam } from "../../controllers/teams/TeamManager";
+
+export class TeamManagerService {
+  async CreateTeamService(req: Request, res: Response) {
+    try {
+      const { name, bio, address, phonenumber, email, avatar, member } = req.body;
+      const createTeam = await CreateTeam({
+        name,
+        bio,
+        address,
+        phonenumber,
+        email,
+        avatar,
+        member,
+      });
+      res.status(createTeam.statusCode).json({
+        message: createTeam.message,
+        data: createTeam.data,
+      });
+    } catch {
+      res.status(500).send({ message: "Error creating team" });
+    }
+  }
+  async UpdateTeamService(req: Request, res: Response) {
+    try {
+      const { id, name, bio, address, phonenumber, email, avatar, member } =
+        req.body;
+      const updateTeam = await UpdateTeam({
+        id,
+        name,
+        bio,
+        address,
+        phonenumber,
+        email,
+        avatar,
+        member,
+      });
+      res.status(updateTeam.statusCode).json({
+        message: updateTeam.message,
+        data: updateTeam.data,
+      });
+    } catch {
+      res.status(500).send({ message: "Error updating team" });
+    }
+  }
+
+  async DeleteTeamService(req: Request, res: Response) {
+    try {
+      const { id } = req.body;
+      const deleteTeam = await DeleteTeam({
+        id
+      })
+      res.status(deleteTeam.statusCode).json({
+        message: deleteTeam.message,
+        data: deleteTeam.data,
+      });
+    } catch {
+      res.status(500).send({ message: "Error delete team" });
+    }
+  }
+}
