f9b77f8d7298603e472c12665563fb895198bed9
calangthang1406 <90557694+carp-calangthang@users.noreply.github.com>
2024-10-21 22:02:45 +0700

    update

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -3,7 +3,7 @@ import * as admin from 'firebase-admin';
 const serviceAccount = require('./serviceAccountKey.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'gs://izytask-79225.appspot.com',
+  storageBucket: `gs://${process.env.STORAGE_BUCKET}`,
 });
 
 const bucket = admin.storage().bucket();
