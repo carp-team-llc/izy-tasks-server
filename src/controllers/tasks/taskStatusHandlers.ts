287091ae94e75b20ec1009e19c66beb199ec0a47
calangthang1406 <90557694+carp-calangthang@users.noreply.github.com>
2024-10-24 00:29:15 +0700

    update rule when change status

--- src/controllers/tasks/taskStatusHandlers.ts
+++ src/controllers/tasks/taskStatusHandlers.ts
@@ -20,6 +20,14 @@ const UpdateTaskStatus = async ({ id, statusKey }) => {
       });
 
       var logStatus = task.statusName
+
+      if (task.status === EnumData.StatusType.Cancel.code && statusKey !== 'New') {
+        return { statusCode: 400, message: "Task is in 'Cancel' state and can only be changed back to 'New'." };
+      }
+  
+      if (task.status === EnumData.StatusType.Completed.code && statusKey === 'Cancel') {
+        return { statusCode: 400, message: "Task is in 'Completed' state and cannot be changed to 'Cancel'." };
+      }
   
       if (task.status === statusInfo.code) {
         return { statusCode: 400, message: `Task is already in the '${statusInfo.code}' state and cannot be changed.` };
